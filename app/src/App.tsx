1a1e0d99795a34e28975d289c9aff0dc6691b272
Yoshitaka motomura <stay.daybreak@gmail.com>
2024-05-15 01:55:23 +0900

    chore: Update App component to display Amazon ECS App version

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Amazon ECS App v1.0</h1>
+        <h1>Amazon ECS App v1.1</h1>
         <p>Deployed via AWS Cloud Development Kit (CDK)</p>
       </div>
     </>
