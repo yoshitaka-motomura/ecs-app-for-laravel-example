692d765ffac7d18ed4afdc70200854c6786f615a
Yoshitaka motomura <stay.daybreak@gmail.com>
2024-05-14 22:09:12 +0900

    chore: Update CDK configuration to use environment variables for AWS account and region

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -6,7 +6,8 @@ import { EcsAppStack } from "../lib/ecs-app-stack";
 const app = new cdk.App();
 new EcsAppStack(app, "EcsAppStack", {
   env: {
-    region: "ap-northeast-1",
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
   },
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
